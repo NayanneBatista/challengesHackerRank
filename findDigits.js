606d6ffbac055af7d22323d849b16b54c3ea865a
Nayanne Batista <nayanne.lb@gmail.com>
2021-01-04 02:06:46 -0300

    Desafio 8

---
+++ findDigits.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the findDigits function below.
+function findDigits(n) {
+
+    let nameN = n + ""
+    let divisores = 0
+
+    if(n > 0 && n < Math.pow(10,9)){
+       for(let i = 0; i < nameN.length; i++){
+            if(n % Number(nameN.charAt(i)) == 0 ){
+                divisores ++
+            }
+        }
+   } 
+    
+    return divisores
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const n = parseInt(readLine(), 10);
+
+        let result = findDigits(n);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
