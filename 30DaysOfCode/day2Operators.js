781321a3ea4f5c0b3f36b08711ad246d3b6dfa01
Nayanne Batista <nayanne.lb@gmail.com>
2021-01-13 10:36:18 -0300

    day2Operators

---
+++ 30DaysOfCode/day2Operators.js
@@ -0,0 +1,42 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function solve(meal_cost, tip_percent, tax_percent) {
+    let tip = (tip_percent/100) * meal_cost;
+    let tax = (tax_percent/100) * meal_cost;
+    let totalCost = meal_cost + tip + tax;
+    
+    console.log(Math.round(totalCost))
+
+}
+
+function main() {
+    const meal_cost = parseFloat(readLine());
+
+    const tip_percent = parseInt(readLine(), 10);
+
+    const tax_percent = parseInt(readLine(), 10);
+
+    solve(meal_cost, tip_percent, tax_percent);
+}
