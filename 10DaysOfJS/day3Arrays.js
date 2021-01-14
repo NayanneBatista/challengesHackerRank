1480d87af9c0c45f02088e056b94e6f8af04781d
Nayanne Batista <nayanne.lb@gmail.com>
2021-01-14 10:33:06 -0300

    day3

---
+++ 10DaysOfJS/day3Arrays.js
@@ -0,0 +1,53 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    
+    let ordenedArray = nums.sort((a, b) => {
+        return a - b
+    })
+    
+    let size = ordenedArray.length
+    let largest = ordenedArray[size -1]
+
+    ordenedArray.splice(ordenedArray.indexOf(largest))
+
+    size = ordenedArray.length
+    let secondLargest = ordenedArray[size -1]
+
+    return secondLargest
+
+}
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
