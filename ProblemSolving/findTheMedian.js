605ca9d600609862dbe749d0b3146cca4ce4fe68
Nayanne Batista <nayanne.lb@gmail.com>
2021-01-21 18:57:27 -0300

    Problem Solving

---
+++ ProblemSolving/findTheMedian.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function findMedian(arr) {
+    let sortedArray = arr.sort( (a, b) => {
+        return (a - b)
+    })
+    
+    let size = arr.length
+    let medianIndex = Math.floor(size/2)
+    let median = sortedArray[medianIndex]
+    return median
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    let result = findMedian(arr);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
