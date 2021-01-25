edcf4a38bbcf9b530af12acd80d0c1f100b270e2
Nayanne Batista <nayanne.lb@gmail.com>
2021-01-24 22:11:46 -0300

    day13

---
+++ 30DaysOfCode/day13AbstractClasses.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var _input = '';
+var _index = 0;
+process.stdin.on('data', (data) => { _input += data; });
+process.stdin.on('end', () => {
+    _input = _input.split(new RegExp('\n'));
+    main();    
+});
+function readLine() { return _input[_index++]; }
+
+/**** Ignore above this line. ****/
+
+class Book {
+    constructor(title, author) {
+        if (this.constructor === Book) {
+            throw new TypeError('Do not attempt to directly instantiate an abstract class.'); 
+        }
+        else {
+            this.title = title;
+            this.author = author;
+        }
+    }
+    
+    display() {
+        console.log('Implement the \'display\' method!')
+    }
+}
+
+class MyBook {
+    constructor(title, author, price){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+    
+    display() {
+        console.log("Title: " + this.title);
+        console.log("Author: " + this.author);
+        console.log("Price: " + this.price);
+    }       
+}
+
+function main() {
+    let title = readLine()
+    let author = readLine()
+    let price = +readLine()
+
+    let book = new MyBook(title, author, price)
+    book.display()
+}
