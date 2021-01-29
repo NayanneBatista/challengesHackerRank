9457e395eccd3aef612330fce087dc2632a3b43c
Nayanne Batista <nayanne.lb@gmail.com>
2021-01-29 11:17:17 -0300

    day 16

---
+++ 30DaysOfCode/day16ExceptionsStringToInteger.js
@@ -0,0 +1,32 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+function main() {
+    const S = readLine();
+    
+    try {
+        const N = Number(S) 
+        Number.isNaN(N) ? err : console.log(N)  
+    }
+    catch (err) {
+        console.log("Bad String")
+    }
+}
+
