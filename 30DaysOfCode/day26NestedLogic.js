b47fe5bc2dfbeb11448334bb0bd785f0b4074217
Nayanne Batista <nayanne.lb@gmail.com>
2021-02-06 23:17:24 -0300

    day 26

--- 30DaysOfCode/day26NestedLogic.js
+++ 30DaysOfCode/day26NestedLogic.js
@@ -1,9 +1,24 @@
 function processData(input) {
     let date = input.split("\n")
-    let dateReturned = date[0]
-    let dateDue = date[1]
+    let returned = date[0]
+    let due = date[1]
+    let dateReturned = returned.split(" ")
+    let dateDue = due.split(" ")
     
-    console.log(dateDue)
+    if(Number(dateReturned[2]) > Number(dateDue[2])) 
+        console.log(10000)
+    else if(Number(dateReturned[2]) < Number(dateDue[2])) 
+        console.log(Number(0))   
+    else if(Number(dateReturned[1]) > Number(dateDue[1]) && (Number(dateDue[2]) - Number(dateReturned[2])) != 1) {
+        let fine = Number(dateReturned[1]) - Number(dateDue[1])
+        console.log(500*fine)
+    }
+    else if(Number(dateReturned[0]) > Number(dateDue[0]) && (Number(dateReturned[1]) - Number(dateDue[1])) != 11) {
+        let fine = Number(dateReturned[0]) - Number(dateDue[0])
+        console.log(15*fine)
+    }
+    else
+        console.log(Number(0))
 } 
 
 process.stdin.resume();
