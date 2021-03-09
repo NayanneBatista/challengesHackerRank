d981d9ec91d80fe4a62fb4c92995d7a48db98c68
Nayanne Batista <nayanne.lb@gmail.com>
2021-03-09 10:26:21 -0300

    day11 - in progress

---
+++ 30DaysOfCode/day11-2DArrays.js
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let arr = Array(6);
+
+    for (let i = 0; i < 6; i++) {
+        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+    
+    let sum = 0
+    
+    for(let i = 0; i < 4; i++){
+        for(let j = 0; j < 4; j++){
+            sum = arr[i][j]
+        }
+    }
+    
+    console.log(sum)
+}
