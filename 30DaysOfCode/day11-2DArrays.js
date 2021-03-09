21ea7091f601b16450959cedf23214fba3097458
Nayanne Batista <nayanne.lb@gmail.com>
2021-03-09 11:22:40 -0300

    day11 all test cases

--- 30DaysOfCode/day11-2DArrays.js
+++ 30DaysOfCode/day11-2DArrays.js
@@ -47,6 +47,44 @@ function main() {
         } 
     }
 
+    hourglasses.sort((a, b) => {
+        return a - b
+    })
+        
+    let maxSum = hourglasses[hourglasses.length - 1]
+    
+    console.log(maxSum)
+}
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let arr = Array(6);
+
+    for (let i = 0; i < 6; i++) {
+        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+    
+    let hourglasses = []
+
+    for (let row = 0; row <= 3; row++) {
+        for (let col = 0; col <= 3; col++) {
+             let sum = 0;
+             
+             sum += arr[row][col];
+             sum += arr[row][col + 1];
+             sum += arr[row][col + 2];
+             sum += arr[row + 1][col + 1];
+             sum += arr[row + 2][col]; 
+             sum += arr[row + 2][col + 1];
+             sum += arr[row + 2][col + 2];
+             
+            hourglasses.push(sum) 
+        } 
+    }
+
     let maxSum = 0
     for(let i = 0; i < hourglasses.length; i++){
         if(hourglasses[i] > maxSum){
