4db5222552c6a7d44d786e30f88467ef6c0c69c8
Nayanne Batista <nayanne.lb@gmail.com>
2021-03-09 10:50:14 -0300

    day11 - tests 6/8

--- 30DaysOfCode/day11-2DArrays.js
+++ 30DaysOfCode/day11-2DArrays.js
@@ -29,13 +29,30 @@ function main() {
         arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
     }
     
-    let sum = 0
-    
-    for(let i = 0; i < 4; i++){
-        for(let j = 0; j < 4; j++){
-            sum = arr[i][j]
+    let hourglasses = []
+
+    for (let row = 0; row <= 3; row++) {
+        for (let col = 0; col <= 3; col++) {
+             let sum = 0;
+             
+             sum += arr[row][col];
+             sum += arr[row][col + 1];
+             sum += arr[row][col + 2];
+             sum += arr[row + 1][col + 1];
+             sum += arr[row + 2][col]; 
+             sum += arr[row + 2][col + 1];
+             sum += arr[row + 2][col + 2];
+             
+            hourglasses.push(sum) 
+        } 
+    }
+
+    let maxSum = 0
+    for(let i = 0; i < hourglasses.length; i++){
+        if(hourglasses[i] > maxSum){
+            maxSum = hourglasses[i]
         }
     }
     
-    console.log(sum)
+    console.log(maxSum)
 }
