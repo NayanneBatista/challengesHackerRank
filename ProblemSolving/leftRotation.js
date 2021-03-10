5cc01dc5b86affd97880ad0d4adc05c60f7e8fdd
Nayanne Batista <nayanne.lb@gmail.com>
2021-03-10 16:06:20 -0300

    Problem Solving

---
+++ ProblemSolving/leftRotation.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function rotateLeft(d, arr) {
+    
+    let i = 0
+    
+    while (i < d) {
+        arr.push(arr.shift())
+        i++
+    }
+    
+    return arr
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const d = parseInt(firstMultipleInput[1], 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = rotateLeft(d, arr);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
